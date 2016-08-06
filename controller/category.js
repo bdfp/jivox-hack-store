579f74798661a75ee57761e0a0800a8dc49bf348
rag594 <raghavrastogi594@gmail.com>
2016-08-06 19:59:23 +0530

    Add category model

---
+++ controller/category.js
@@ -0,0 +1,65 @@
+/**
+ * Created by raghav on 06/08/16.
+ */
+
+var pool = require('../lib/pool');
+var mysql = require('mysql');
+
+
+var category = {
+
+    addCategory (category, cb) {
+        if (!category.name || !category.vendor_id) {
+            return cb("Parameter missing or unauthorized");
+        }
+        this.getCategory (category.name, (err, categories) => {
+            if (err || categories.length > 0) {
+                cb(err || "category already exists");
+            } else {
+                pool.getConnection((err, conn) => {
+                    if (err) {
+                        cb(err, null);
+                    } else {
+                        var query = mysql.format('INSERT INTO category_details SET ?',category);
+                        conn.query(query, (err, result) => {
+                            conn.release();
+                            cb(err);
+                        })
+                    }
+                });
+            }
+        });
+
+    },
+    getCategory (name, cb) {
+        pool.getConnection((err, conn) => {
+            if (err) {
+                cb(err, null);
+            } else {
+                var query = mysql.format('SELECT * FROM category_details WHERE name = ?', name );
+                conn.query(query, (err, rows) => {
+                    conn.release();
+                    cb(err, rows);
+                });
+            }
+        });
+    },
+
+    getAllCategories(cb) {
+        pool.getConnection((err, conn) => {
+            if (err) {
+                cb(err, null);
+            } else {
+                conn.query('SELECT * FROM category_details', (err, rows) => {
+                    conn.release();
+                    if (err) {
+                        return cb(err,null);
+                    }
+                    cb(err,rows);
+                })
+            }
+        });
+    }
+};
+
+module.exports = category;
---
+++ controller/labels.js
@@ -0,0 +1,59 @@
+/**
+ * Created by raghav on 06/08/16.
+ */
+
+var pool = require('../lib/pool');
+var mysql = require('mysql');
+
+
+var labels = {
+
+    addLabel(label,cb) {
+        if (!label.name) {
+            return cb("Parameter missing");
+        }
+        this.getLabel(label.name, (err, labels) => {
+            if(err || labels.length > 0){
+                cb(err || "label already exists");
+            } else {
+                pool.getConnection((err, conn) => {
+                   if (err) {
+                       cb(err, null);
+                   } else {
+                       var query = mysql.format('INSERT INTO labels_details SET ?',label);
+                       conn.query(query, (err, result) => {
+                          conn.release();
+                           cb(err);
+                       })
+                   }
+                });
+            }
+        });
+
+    },
+    getLabel(name,cb) {
+        pool.getConnection((err, conn) => {
+            if (err) {
+                cb(err, null);
+            } else {
+                var query = mysql.format('SELECT * FROM labels_details WHERE name = ?', name );
+                conn.query(query, (err, rows) => {
+                   conn.release();
+                    cb(err,rows);
+                });
+            }
+        });
+    },
+
+    getAllLabels(cb) {
+        pool.getConnection((err, conn) => {
+           if (err) {
+               cb(err, null);
+           } else {
+
+           }
+        });
+    }
+};
+
+module.exports = label;
