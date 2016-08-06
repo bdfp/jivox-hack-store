7676c8cb5188475ff642eb59e54a45b6aa33efac
Shreyansh Nahata <shreyans4nahata@gmail.com>
2016-08-07 02:56:20 +0530

    Address addition

--- public/src/user/profile/ProfileController.js
+++ public/src/user/profile/ProfileController.js
@@ -1,7 +1,18 @@
-/*(function () {
+(function () {
 	'use strict';
 
-	angular.module('jivox')
-		.controller('ProfileController', function())
+	angular.module('jivoxHack')
+		.controller('ProfileController', function($http, $window, $stateParam){
+			var self = this;
 
-})*/
+			self.update = function(){
+				Profile.postAddress(data)
+					.then(function(response){
+						console.log(response);
+					}).catch(function(reason){
+						console.log(reason);
+					});
+			};
+		});
+
+})();
